46da47d304ea1cee96354188f073343cbde7f413
Jacquie24 <jgcaption@yahoo.com>
2020-12-17 17:45:43 -0500

    Cleaned up code.

--- script.js
+++ script.js
@@ -38,8 +38,7 @@ $(document).ready(function () {
 
   var arrayOfHours = [09, 10, 11, 12, 13, 14, 15, 16, 17];
 
-
-  
+//   Loop through arrayOfHours and add rows and columns.
   for (i = 0; i < arrayOfHours.length; i++) {
     // create row
     var rowEl = $("<div>");
@@ -49,17 +48,14 @@ $(document).ready(function () {
     var timeColEl = $("<div>");
     timeColEl.addClass("col-sm-1 hour");
     if (arrayOfHours[i] < 12) {
-        var amHours = arrayOfHours[i] + "AM";
-        timeColEl.append(amHours);
-    } else if 
-        (arrayOfHours[i] === 12) {
-            var noonHour = "12PM"
-            timeColEl.append(noonHour);
-        
-    
+      var amHours = arrayOfHours[i] + "AM";
+      timeColEl.append(amHours);
+    } else if (arrayOfHours[i] === 12) {
+      var noonHour = "12PM";
+      timeColEl.append(noonHour);
     } else {
-        var pmHours = parseInt(arrayOfHours[i]) - 12 + "PM";
-        timeColEl.append(pmHours);
+      var pmHours = parseInt(arrayOfHours[i]) - 12 + "PM";
+      timeColEl.append(pmHours);
     }
     rowEl.append(timeColEl);
 
@@ -67,15 +63,14 @@ $(document).ready(function () {
     var textEl = $("<textarea>");
     textEl.attr("id", "hour" + arrayOfHours[i]);
 
+    // insert if statement for hours to match with assigned class to change colors
     if (arrayOfHours[i] > hourDay) {
       console.log("Time is present");
       textEl.addClass("col-sm-10 description future");
-      
     } else if (arrayOfHours[i] < hourDay) {
       textEl.addClass("col-sm-10 description past");
     } else {
-        textEl.addClass("col-sm-10 description present");
-
+      textEl.addClass("col-sm-10 description present");
     }
     rowEl.append(textEl);
 
@@ -84,41 +79,27 @@ $(document).ready(function () {
     buttonColEl.addClass("col-1 saveBtn fas fa-save");
     rowEl.append(buttonColEl);
 
-    
-
     containerEl.append(rowEl);
     buttonColEl.on("click", saveEvent);
     console.log("you clicked a button");
-
-// assign textContent to what?
-// append textContent to textEl?
-
-    
-
-    }
-  
-
+  }
+// Save text area to local storage after button click.
   function saveEvent(event) {
     event.preventDefault();
-    console.log($(this).siblings(".hour").text());
-    console.log($(this).siblings(".description").val());
-    var hour = ($(this).siblings(".hour").text());
-    var userEntry = ($(this).siblings(".description").val());
-  localStorage.setItem(hour, userEntry);
+    var hour = $(this).siblings(".hour").text();
+    var userEntry = $(this).siblings(".description").val();
+    localStorage.setItem(hour, userEntry);
 
-//   var enteredEvents = getEvents();
-//   //save the event to storage
-//   localStorage.setItem("events", JSON.stringify(enteredEvents));
   }
 
-  function getEvents() {
-    var workdayEvents = JSON.parse(localStorage.getItem("events"));
-    return workdayEvents;
-  }
-
-  getEvents();
-
+// Retrieve from local storage by correct hour.
   $("#hour9").val(localStorage.getItem("9AM"));
-
-  
+  $("#hour10").val(localStorage.getItem("10AM"));
+  $("#hour11").val(localStorage.getItem("11AM"));
+  $("#hour12").val(localStorage.getItem("12PM"));
+  $("#hour13").val(localStorage.getItem("1PM"));
+  $("#hour14").val(localStorage.getItem("2PM"));
+  $("#hour15").val(localStorage.getItem("3PM"));
+  $("#hour16").val(localStorage.getItem("4PM"));
+  $("#hour17").val(localStorage.getItem("5PM"));
 });
