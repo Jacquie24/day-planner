3460a1423bba77380434749eeade6b43cfdb12c9
Jacquie24 <jgcaption@yahoo.com>
2020-12-16 20:24:13 -0500

    Built rows in JS instead of HTML

--- script.js
+++ script.js
@@ -1,48 +1,72 @@
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
-    // a. momentsjs connection
+// a. momentsjs connection
 
 // WHEN I scroll down
 // THEN I am presented with time blocks for standard business hours
-    // This is our HTML lineup
+// This is our HTML lineup
 
 // WHEN I view the time blocks for that day
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
-    // Javascript to style these blocks
-    // And momentsjs connection again to compare time
-    // Each block associated with a time
+// Javascript to style these blocks
+// And momentsjs connection again to compare time
+// Each block associated with a time
 
 // WHEN I click into a time block
 // THEN I can enter an event
-    // HTML element is a field
+// HTML element is a field
 
 // WHEN I click the save button for that time block
 // THEN the text for that event is saved in local storage
-    // .on submit
+// .on submit
 
 // WHEN I refresh the page
 // THEN the saved events persist
-    // populate screen from DOM storage
-    // event preventDefault
+// populate screen from DOM storage
+// event preventDefault
 
-$(document).ready(function() {
-    console.log("Document is ready");
-})
-checkTime();
+$(document).ready(function () {
+  console.log("Document is ready");
+});
 
-// var arrayOfHours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
+var topDate = moment().format("dddd, MMMM Do");
+$("#currentDay").append(topDate);
 
-for (i = 9; i < 18; i++) {
-  var hourEl = "#hr-" + i;
-  if (i === moment().hour()) {
-      $(hourEl).addClass(".present");
-  } else if (i < moment().hour()) {
-    $(hourEl).addClass(".past");
+var containerEl = $(".container");
+
+var arrayOfHours = [09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+for (i = 0; i < arrayOfHours.length; i++) {
+  // create row
+  var rowEl = $("<div>");
+  rowEl.addClass("row time-block");
+
+  // create the column for time
+  var timeColEl = $("<div>");
+  timeColEl.addClass("col-sm-1 hour");
+
+  if (arrayOfHours[i] === moment().hour()) {
+    console.log("Time is present");
+    $(timeColEl).addClass(".present");
+  } else if (arrayOfHours[i] < moment().hour()) {
+    $(timeColEl).addClass(".past");
   } else {
-      $(hourEl).addClass(".future");
-  }
+    $(timeColEl).addClass(".future");
+rowEl.append(timeColEl);
+}
 }
 
-function checkTime () {
-    moment().hour();
-}
+// create column for text
+var textEl = $("<textarea>");
+textEl.addClass("col-sm-10 description");
+rowEl.append(textEl);
+
+// create column for save
+var buttonColEl = $("<button>");
+buttonColEl.addClass("col-1 saveBtn fas-fas-save");
+rowEl.append(buttonColEl);
+
+containerEl.append(rowEl);
+
+//   }
+// }
